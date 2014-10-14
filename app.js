5edb8e93388cd3bbfb6b1c57b8a9060ea8164752
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-14 17:23:23 -0600

    fixed functionality & squished bugs

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.post('/formsubmit', function(req, res){
+	res.redirect('/success');
+});
+
+app.get('/success', function(req, res){
+	res.render('success');
+});
+
 var server = app.listen(9803, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
